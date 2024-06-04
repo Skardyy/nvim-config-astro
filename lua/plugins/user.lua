7b97ed73e6a6d64b43cdab2c635ff75453de74f9
skardy <meronb1312@gmail.com>
2024-06-04 18:36:00 +0300

    changed the theme massively

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -2,10 +2,68 @@
 return {
   {
     "dracula/vim",
+    -- config = function()
+    --   vim.cmd [[colorscheme dracula]]
+    --   vim.cmd [[
+    --     hi Normal guibg=None ctermbg=None
+    --     hi ColorColumn guibg=None ctermbg=None
+    --     hi SignColumn guibg=None ctermbg=None
+    --     hi Folded guibg=None ctermbg=None
+    --     hi FoldColumn guibg=None ctermbg=None
+    --     hi CursorLine guibg=None ctermbg=None
+    --     hi CursorColumn guibg=None ctermbg=None
+    --     hi WhichKeyFloat guibg=None ctermbg=None
+    --     hi VertSplit guibg=None ctermbg=None
+    --     hi DiffChange guibg=None ctermbg=None
+    --
+    --     hi @punctuation ctermfg=white
+    --   ]]
+    -- end,
+  },
+  {
+    "Shatur/neovim-ayu",
+    -- config = function()
+    --   vim.cmd [[colorscheme ayu]]
+    -- vim.cmd [[
+    --   hi Normal guibg=None ctermbg=None
+    --   hi ColorColumn guibg=None ctermbg=None
+    --   hi SignColumn guibg=None ctermbg=None
+    --   hi Folded guibg=None ctermbg=None
+    --   hi FoldColumn guibg=None ctermbg=None
+    --   hi CursorLine guibg=#303030 ctermbg=None
+    --   hi CursorColumn guibg=None ctermbg=None
+    --   hi WhichKeyFloat guibg=None ctermbg=None
+    --   hi VertSplit guibg=None ctermbg=None
+    --   hi DiffChange guibg=None ctermbg=None
+    --   hi LineNr guibg=None ctermbg=None
+    --   hi StatusLine guibg=None ctermbg=None
+    --   hi CursorLineNr guibg=None ctermbg=None
+    --
+    --   hi @punctuation ctermfg=white
+    -- ]]
+    -- end,
+  },
+  {
+    "tanvirtin/monokai.nvim",
     config = function()
-      vim.cmd [[colorscheme dracula]]
-      vim.cmd "hi Normal guibg=NONE ctermbg=NONE"
-      vim.cmd "hi TabLineFill guibg=NONE ctermbg=NONE"
+      vim.cmd [[colorscheme monokai]]
+      vim.cmd [[
+        hi Normal guibg=None ctermbg=None
+        hi ColorColumn guibg=None ctermbg=None
+        hi SignColumn guibg=None ctermbg=None
+        hi Folded guibg=None ctermbg=None
+        hi FoldColumn guibg=None ctermbg=None
+        hi CursorLine guibg=#303030 ctermbg=None
+        hi CursorColumn guibg=None ctermbg=None
+        hi WhichKeyFloat guibg=None ctermbg=None
+        hi VertSplit guibg=None ctermbg=None
+        hi DiffChange guibg=None ctermbg=None
+        hi LineNr guibg=None ctermbg=None
+        hi StatusLine guibg=None ctermbg=None
+        hi CursorLineNr guibg=None ctermbg=None
+
+        hi @punctuation ctermfg=white
+      ]]
     end,
   },
 }
